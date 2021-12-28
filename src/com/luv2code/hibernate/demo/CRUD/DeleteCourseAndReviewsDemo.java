c5cc45a48dcea43e0febd513156deff55a473f7e
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2021-12-28 18:52:22 +0000

    One-to-many unidirectional CRUD

--- src/com/luv2code/hibernate/demo/CRUD/DeleteCourseAndReviewsDemo.java
+++ src/com/luv2code/hibernate/demo/CRUD/DeleteCourseAndReviewsDemo.java
@@ -8,7 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class GetCourseReviewsDemo {
+public class DeleteCourseAndReviewsDemo {
     public static void main(String[] args) {
         //create session factory
         SessionFactory factory = new Configuration()
@@ -30,6 +30,8 @@ public class GetCourseReviewsDemo {
             System.out.println("Found Course: " + tempCourse);
             System.out.println("Associated Reviews: " + tempCourse.getReviews());
 
+
+            session.delete(tempCourse);
             //commit the transaction
             session.getTransaction().commit();
             System.out.println("Complete");
---
+++ src/com/luv2code/hibernate/demo/CRUD/GetCourseAndReviewsDemo.java
@@ -0,0 +1,44 @@
+package com.luv2code.hibernate.demo.CRUD;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCourseAndReviewsDemo {
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try{
+            // start the transaction
+            session.beginTransaction();
+
+            int courseId = 10;
+            Course tempCourse = session.get(Course.class, courseId);
+            System.out.println("Found Course: " + tempCourse);
+            System.out.println("Associated Reviews: " + tempCourse.getReviews());
+
+            //commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Complete");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
